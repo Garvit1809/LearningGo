ce388a03394f0c8e1ea15b7581821c0eefbba040
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-09-07 23:25:04 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	conferenceName := "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets int = 50
+
+	fmt.Printf("Welcome to the %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still remaining\n", conferenceTickets, remainingTickets)
+	fmt.Println(("Get your tickets here to attend"))
+
+	var username string
+	var userTickets int
+
+	// var bookings [50]string
+
+	//  Pointer --> &
+	fmt.Println("Enter your name:-")
+	fmt.Scan(&username)
+	fmt.Println("Enter the tickets you want to book:-")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("User %v booked %v tickets", username, userTickets)
+	fmt.Printf("We have %v tickets left", remainingTickets)
+}
